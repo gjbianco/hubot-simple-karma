fff4027a37f0bc4343956744048e318f8ff340c1
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-02-25 16:00:11 -0500

    handle single digit modifier

--- src/hubot-simple-karma.js
+++ src/hubot-simple-karma.js
@@ -1,63 +1,75 @@
-const _ = require('lodash')
+const _ = require('lodash');
 
-const KARMA_PREFIX = process.env.HUBOT_KARMA_PREFIX || 'hubot-simple-karma'
+const KARMA_PREFIX = process.env.HUBOT_KARMA_PREFIX || 'hubot-simple-karma';
 
-module.exports = (robot) => {
-
+module.exports = robot => {
   // chat hooks -------------------------------
 
-  robot.hear(/^(.+)(\+\+|--)$/i, (res) => {
-    const name = res.match[1].trim()
-    const karmas = openKarmas()
-    let val = karmas[name] || 0
-    val += res.match[2] === '++' ? 1 : -1
-    if (val) {
-      karmas[name] = val
-    } else {
-      delete karmas[name]
-    }
-    closeKarmas(karmas)
-    res.send(formatKarma(name, karmas[name]))
-  })
+  robot.hear(/^(.+)(\+\+|--)$/i, res => {
+    const name = res.match[1];
+    const amount = res.match[2] === '++' ? 1 : -1;
+    changeKarma(name, amount);
+  });
 
-  robot.hear(/^!karma(?: (.+))?$/i, (res) => {
-    const name = res.match[1]
-    const karmas = openKarmas()
+  robot.hear(/^(.+?)([+-]\d)$/i, res => {
+    const name = res.match[1];
+    const amount = parseInt(res.match[2], 10);
+    changeKarma(name, amount);
+  });
+
+  robot.hear(/^!karma(?: (.+))?$/i, res => {
+    const name = res.match[1];
+    const karmas = openKarmas();
     if (name) {
-      res.send(formatKarma(name, karmas[name]))
+      res.send(formatKarma(name, karmas[name]));
     } else {
-      let kTable = ''
+      let kTable = '';
       const topList = _.chain(karmas)
-                       .toPairs()
-                       .orderBy('1', 'desc')
-                       .slice(0, 10)
-                       .value()
+        .toPairs()
+        .orderBy('1', 'desc')
+        .slice(0, 10)
+        .value();
       _.each(topList, (row, index) => {
-        kTable += `\t${index + 1}: (${row[1]})\t${row[0]}\n`
-      })
-      res.send('top ten:\n' + kTable)
+        kTable += `\t${index + 1}: (${row[1]})\t${row[0]}\n`;
+      });
+      res.send('top ten:\n' + kTable);
     }
-  })
+  });
 
   // utility functions ---------------------------
 
-  function openKarmas () {
-    return robot.brain.get(KARMA_PREFIX)
+  function changeKarma(name, isPositive, amount) {
+    const karmas = openKarmas();
+
+    name = name.trim();
+    let val = karmas[name] || 0;
+    val += amount;
+    if (val) {
+      karmas[name] = val;
+    } else {
+      delete karmas[name];
+    }
+
+    closeKarmas(karmas);
+    res.send(formatKarma(name, karmas[name]));
   }
 
-  function closeKarmas (karmas) {
-    robot.brain.set(KARMA_PREFIX, karmas)
+  function openKarmas() {
+    return robot.brain.get(KARMA_PREFIX);
   }
 
-  function formatKarma (name, karma) {
-    return `${name} has ${karma || 'no'} karma`
+  function closeKarmas(karmas) {
+    robot.brain.set(KARMA_PREFIX, karmas);
+  }
+
+  function formatKarma(name, karma) {
+    return `${name} has ${karma || 'no'} karma`;
   }
 
   // initialize data, if necessary
   if (!openKarmas()) {
     closeKarmas({
-      nobody: 1
-    })
+      nobody: 1,
+    });
   }
-
-}
+};
